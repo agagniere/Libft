d1ab637a3a9d7be29651c5282c8550b43560ba9f
Antoine GAGNIERE <angagnie@student.42.fr>
2015-12-14 18:54:33 +0100

    RageQuit

--- include/ft_matrix.h
+++ include/ft_matrix.h
@@ -6,34 +6,74 @@
 /*   By: angagnie <angagnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/14 12:41:58 by angagnie          #+#    #+#             */
-/*   Updated: 2015/12/14 12:42:17 by angagnie         ###   ########.fr       */
+/*   Updated: 2015/12/14 17:19:01 by angagnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_MATRIX_H
 # define FT_MATRIX_H
 
-typedef struct		s_vectf2
-{
-	float			x;
-	float			y;
-}					t_vectf2;
+# define ORIGIN2 (t_point2){0,0}
 
-typedef struct		s_vecti2
+typedef union	u_vect2
 {
-	int				x;
-	int				y;
-}					t_vecti2;
+	struct
+	{
+		float	x;
+		float	y;
+	}			coord;
+	float		table[2];
+}				t_vect2;
 
-typedef struct		s_vect2
+typedef union	u_point2
 {
-	size_t			x;
-	size_t			y;
-}					t_vect2;
+	struct
+	{
+		size_t	x;
+		size_t	y;
+	}			coord;
+	size_t		table[2];
+}				t_point2;
 
-typedef struct		s_mat2x2
+typedef union	u_vect3
 {
-	size_t			data[4];
-}					t_mat2x2;
+	struct
+	{
+		float	x;
+		float	y;
+		float	z;
+	}			coord;
+	float		table[3];
+}				t_vect3;
+
+typedef union	u_vect4
+{
+	struct
+	{
+		float	x;
+		float	y;
+		float	z;
+		float	w;
+	}			coord;
+	float		table[4];
+}				t_vect4;
+
+typedef union	u_mat2x2
+{
+	t_vect2		col[2];
+	float		table[4];
+}				t_mat2x2;
+
+typedef union	u_mat3x3
+{
+	t_vect3		col[3];
+	float		table[9];
+}				t_mat3x3;
+
+typedef union	u_mat4x4
+{
+	t_vect4		col[4];
+	float		table[16];
+}				t_mat4x4;
 
 #endif
--- src/ft_math/ft_fib.c
+++ src/ft_math/ft_fib.c
@@ -6,41 +6,46 @@
 /*   By: angagnie <angagnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/14 11:24:31 by angagnie          #+#    #+#             */
-/*   Updated: 2015/12/14 13:00:55 by angagnie         ###   ########.fr       */
+/*   Updated: 2015/12/14 15:02:03 by angagnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-??=include "ft_math.h"
+#include "ft_math.h"
+
+t_matrix	ft_mat2x2_times_mat2x2()
+{
+
+}
 
 /*
 ** EFFETS DE BORD
 */
-
-/*void	ft_mat2x2_times_mat2x2()
-{
-}*/
-
 void	ft_mat2x2_pow(t_mat2x2 *m, size_t p)
-??<
+{
 	t_mat2x2	s;
 
 	if (p == 0)
-	??<
-		m->data??(0??) = 1;
-		m->data??(1??) = 0;
-		m->data??(2??) = 0;
-		m->data??(3??) = 1;
-	??>
+	{
+		m->data[0] = 1;
+		m->data[1] = 0;
+		m->data[2] = 0;
+		m->data[3] = 1;
+	}
 	else if (p > 1 && p % 2)
 	{
 		s = *m;
 		ft_mat2x2_pow(m, p - 1);
-//		ft_mat2x2_times_mat2x2(m, s);
+		ft_mat2x2_mult_mat2x2(m, s);
 	}
-??>
+}
 
 
-size_t	ft_fib(unsigned char n)
+/*
+** This function has a O(log(n)) complexity,
+** and is to be prefered when n > 30
+*/
+
+size_t	ft_fib2(unsigned char n)
 {
 	t_vect2		f;
 	t_mat2x2	m;
@@ -49,8 +54,25 @@ size_t	ft_fib(unsigned char n)
 		return (0);
 	f.x = 1;
 	f.y = 1;
-	m.data??(0??) = 1;
-	m.data??(1??) = 1;
-	m.data??(2??) = 1;
-	m.data??(3??) = 0;
+	m.data[0] = 1;
+	m.data[1] = 1;
+	m.data[2] = 1;
+	m.data[3] = 0;
+}
+
+unsigned int	ft_fib(unsigned char n)
+{
+	unsigned int	fn;
+	unsigned int	fnm1;
+	unsigned int	fnm2;
+
+	fn = 1;
+	fnm1 = 1;
+	while (n-- > 1)
+	{
+		fnm2 = fnm1;
+		fnm1 = fn;
+		fn = fnm1 + fnm2;
+	}
+	return (fn);
 }

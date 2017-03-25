b880815c6ad8dcc6b539f11d23740836586f49f5
Xxdzs <sid.xxdzs@gmail.com>
2017-03-25 04:28:55 +0100

    tiny modification

--- include/ft_token.h
+++ include/ft_token.h
@@ -6,7 +6,7 @@
 /*   By: angagnie <angagnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/29 05:43:19 by angagnie          #+#    #+#             */
-/*   Updated: 2017/03/19 05:57:14 by angagnie         ###   ########.fr       */
+/*   Updated: 2017/03/25 02:58:36 by angagnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,24 @@
 # include "ft_stream.h"
 
 typedef struct s_tokenizer	t_tokenizer;
+typedef struct s_token		t_token;
+
+struct						s_token
+{
+	uint8_t		tag;
+	t_substr	data;
+};
 
 struct						s_tokenizer
 {
 	t_is		*in;
-	t_string	current[1];
-	uint8_t		tag;
+	t_token		current;
 	int			eof;
 };
 
-# define NEW_TOKENIZER(IS) (t_tokenizer){IS, {NULL, 0}, 0, 0}
+# define NEW_TOKENIZER(IS) (t_tokenizer){IS, NEW_TOKEN, 0}
+# define NEW_TOKEN (t_token){0, {NULL, 0}}
 
 # define TK_SKIP(TK, N) ((TK)->in->buff_i += N)
-
-# define TK_ACCEPT(TK, C) (fta_append((TK)->current, &(C), 1))
-
-# define TK_EMPTY(TK) ((TK)->current.size = 0)
 
 #endif

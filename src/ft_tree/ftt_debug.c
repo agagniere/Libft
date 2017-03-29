401007c6dcf113eea79c08d286d09bcf47fb604d
Xxdzs <sid.xxdzs@gmail.com>
2017-03-29 07:40:52 +0200

    Tiny improvement

--- src/ft_tree/ftt_debug.c
+++ src/ft_tree/ftt_debug.c
@@ -6,24 +6,24 @@
 /*   By: angagnie <angagnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/28 21:53:52 by angagnie          #+#    #+#             */
-/*   Updated: 2017/03/29 05:36:32 by angagnie         ###   ########.fr       */
+/*   Updated: 2017/03/29 07:40:24 by angagnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_tree.h"
 #include <stdio.h>
 
-void	node_debug(t_tnode *self, int depth)
+void	node_debug(t_tnode *self, int depth, char c)
 {
-	printf("%.*s(%p, %#x) --- %p, %p\n", depth, " ", (void *)self, self->label, self->left, self->right);
+	printf("%*s%c(%p, %#x) --- %p, %p\n", depth, "", c, (void *)self, self->label, self->left, self->right);
 	if (self->left)
-		node_debug(self->left, depth + 1);
-	if (self->left)
-		node_debug(self->left, depth + 1);
+		node_debug(self->left, depth + 1, 'L');
+	if (self->right)
+		node_debug(self->right, depth + 1, 'R');
 }
 
 void	ftt_debug(t_tree *self)
 {
 	printf("This tree has %i nodes.\n", self->count);
-	node_debug(self->root, 0);
+	node_debug(self->root, 0, 'R');
 }

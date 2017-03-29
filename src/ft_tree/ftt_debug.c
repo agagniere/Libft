41549d918c9884c42cb780fd640debbaaf55eae4
Xxdzs <sid.xxdzs@gmail.com>
2017-03-29 05:37:18 +0200

    Hotfix

--- src/ft_tree/ftt_debug.c
+++ src/ft_tree/ftt_debug.c
@@ -6,7 +6,7 @@
 /*   By: angagnie <angagnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/28 21:53:52 by angagnie          #+#    #+#             */
-/*   Updated: 2017/03/28 22:53:02 by angagnie         ###   ########.fr       */
+/*   Updated: 2017/03/29 05:36:32 by angagnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 void	node_debug(t_tnode *self, int depth)
 {
-	printf("%.*s(%p, %#x) --- %p, %p", depth, " ", (void *)self, self->label, self->left, self->right);
+	printf("%.*s(%p, %#x) --- %p, %p\n", depth, " ", (void *)self, self->label, self->left, self->right);
 	if (self->left)
 		node_debug(self->left, depth + 1);
 	if (self->left)
@@ -24,6 +24,6 @@ void	node_debug(t_tnode *self, int depth)
 
 void	ftt_debug(t_tree *self)
 {
-	printf("This tree has %i nodes.", self->count);
+	printf("This tree has %i nodes.\n", self->count);
 	node_debug(self->root, 0);
 }

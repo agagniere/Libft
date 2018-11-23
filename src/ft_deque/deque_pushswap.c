3494ca139413db37a419e984a7470f45e52199a1
Antoine GAGNIERE <sid.xxdzs@gmail.com>
2018-11-23 19:20:21 +0100

    Add pushswap specific functions

---
+++ src/ft_deque/deque_pushswap.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   deque_pushswap.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: angagnie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/23 15:32:49 by angagnie          #+#    #+#             */
+/*   Updated: 2018/11/23 18:38:32 by angagnie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdint.h>
+
+#include "ft_stack.h"
+#include "ft_deque_private.h"
+
+bool	stack_swap(t_stack *self)
+{
+	void		*penu;
+	void		*last;
+	void		*temp;
+
+	if (ftq_size(self) < 2)
+		return (false);
+	temp = ftq_back(self);
+	last = stack_peek(self);
+	penu = last;
+	FTQ_MOVE_BACKWARD_ONE(self, penultimate);
+	ft_memmove(temp, last, self->type_size);
+	ft_memmove(last, penu, self->type_size);
+	ft_memmove(penu, temp, self->type_size);
+	return (true);
+}
+
+bool	stack_rotate(t_stack *self)
+{
+	if (ftq_size(self) < 2)
+		return (false);
+	stack_pop(self, NULL);
+	FTQ_PUSH_FRONT_ONE(self, ftq_back(self));
+}
+
+bool	stack_rotate_reverse(t_stack *self)
+{
+	void	*temp;
+
+	temp = ftq_first(self);
+	FTQ_POP_FRONT_ONE(self, NULL);
+	stack_push(self, temp);
+}

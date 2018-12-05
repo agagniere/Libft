bf56c0dfa67660a6f0da688606a3572b4f97fbe1
Antoine GAGNIERE <sid.xxdzs@gmail.com>
2018-12-05 17:45:58 +0100

    Added bitset header

---
+++ include/ft_bitset.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bitset.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: angagnie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/05 17:18:04 by angagnie          #+#    #+#             */
+/*   Updated: 2018/12/05 17:44:50 by angagnie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BITSET_H
+# define FT_BITSET_H
+
+# include <stdint.h>
+
+typedef uint64_t		t_bitset;
+
+// Mmmmhh
+//# define BITSET_NEW(NAME, SIZE) t_bitset NAME[(SIZE) / sizeof(t_bitset)]
+
+# define BITSET_GET(S,V) (S[BS_INDEX(V)] & BS_MASK(V))
+# define BITSET_SET(S,V) (S[BS_INDEX(V)] |= BS_MASK(V))
+
+/*
+** ----------===== private: =====----------
+*/
+
+# define BS_ONE ((t_bitset)1)
+
+# define BS_BIT_PER_ITEM (sizeof(t_bitset) << 3)
+
+# define BS_INDEX(V) ((V) / BS_BIT_PER_ITEM)
+
+# define BS_BIT(V) ((V) % BS_BIT_PER_ITEM)
+
+# define BS_MASK(V) (BS_ONE << BS_BIT(V))
+
+#endif

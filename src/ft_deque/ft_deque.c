07ef21a88fa50854689111e0f9c3b09c26e47539
Antoine GAGNIERE <sid.xxdzs@gmail.com>
2018-12-05 15:08:49 +0100

    Deque public functions now conform to the 42 Norm

--- src/ft_deque/ft_deque.c
+++ src/ft_deque/ft_deque.c
@@ -6,30 +6,32 @@
 /*   By: angagnie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/23 13:32:56 by angagnie          #+#    #+#             */
-/*   Updated: 2018/11/23 18:44:15 by angagnie         ###   ########.fr       */
+/*   Updated: 2018/12/05 15:07:07 by angagnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_deque.h"
 #include "ft_deque_private.h"
-#include "libft.h"
 
 /*
 ** Returns an initialized t_deque that uses the specified storage
 */
+
 t_deque		ftq_new(void *data, size_t type_size, unsigned capacity)
 {
 	return ((t_deque){data, data, data, type_size, capacity});
 }
 
 /*
-** Before pushing any element, we can clarify our intentions to lower the chances of splitting data
+** Before pushing any element,
+** we can clarify our intentions to lower the chances of splitting data
 ** 'B' : will only push back
 ** 'b' : will mostly push back
 ** '2' : will push on both side
 ** 'f' : will mostly push front
 ** 'F' : will only push front
 */
+
 bool		ftq_intent(t_deque *self, char intent)
 {
 	if (!ftq_is_empty(self))
@@ -46,40 +48,8 @@ bool		ftq_intent(t_deque *self, char intent)
 		self->front = ftq_end(self) - ftq_offset(self, 1);
 	else
 		return (false);
-	}
 	self->back = self->front;
 	return (true);
-}
-
-void		*ftq_first(t_deque *self)
-{
-	return (ftq_is_empty(self) ? NULL : self->front);
-}
-
-void		*ftq_last(t_deque *self)
-{
-	if (ftq_is_empty(self))
-		return (NULL);
-	return (((self->back == ftq_begin(self)) ? ftq_end(self) : self->back)
-			- ftq_offset(self, 1));
-}
-
-/*
-** Number of elements stored
-*/
-
-unsigned	ftq_size(t_deque *self)
-{
-	return (ftq_is_split(self) ? ftq_exterior(self) : ftq_interior(self));
-}
-
-/*
-** Maximum number of elements that can be stored
-*/
-
-unsigned	ftq_max(t_deque *self)
-{
-	return (self->capacity - 1);
 }
 
 bool		ftq_is_empty(t_deque *self)
---
+++ src/ft_deque/ftq_getters.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ftq_getters.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: angagnie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/23 13:32:56 by angagnie          #+#    #+#             */
+/*   Updated: 2018/12/05 14:58:00 by angagnie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_deque.h"
+#include "ft_deque_private.h"
+
+void		*ftq_first(t_deque *self)
+{
+	return (ftq_is_empty(self) ? NULL : self->front);
+}
+
+void		*ftq_last(t_deque *self)
+{
+	if (ftq_is_empty(self))
+		return (NULL);
+	return (((self->back == ftq_begin(self)) ? ftq_end(self) : self->back)
+			- ftq_offset(self, 1));
+}
+
+/*
+** Number of elements stored
+*/
+
+unsigned	ftq_size(t_deque *self)
+{
+	return (ftq_is_split(self) ? ftq_exterior(self) : ftq_interior(self));
+}
+
+/*
+** Maximum number of elements that can be stored
+*/
+
+unsigned	ftq_max(t_deque *self)
+{
+	return (self->capacity - 1);
+}

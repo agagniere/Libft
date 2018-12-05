c9a615e0640bdb9785912ebd13afb50a50381f18
Antoine GAGNIERE <sid.xxdzs@gmail.com>
2018-12-05 19:08:50 +0100

    Improved the Array::toString function It now frees resources after using them And uses the string wraper to be more explicit

--- src/ft_array/fta_string.c
+++ src/ft_array/fta_string.c
@@ -6,14 +6,16 @@
 /*   By: angagnie <angagnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/18 18:09:32 by angagnie          #+#    #+#             */
-/*   Updated: 2017/02/27 19:15:34 by angagnie         ###   ########.fr       */
+/*   Updated: 2018/12/05 19:00:59 by angagnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_array.h"
-#include "libft.h"
+#include "ft_string.h"
 
-#define TMP_STRING_APPEND(A,S) fta_append(A, S, ft_strlen(S))
+#include <stdlib.h>
+
+#define STRING_APPEND(A,S) fta_append(A, S, ft_strlen(S))
 
 /*
 ** Array::string
@@ -28,24 +30,22 @@
 
 char	*fta_string(t_array *self, char *(*f)(void *))
 {
-	t_array		ans[1];
+	t_string	ans[1];
 	void		*iterator;
 	char		*tmp;
 
-	*ans = NEW_ARRAY(char);
-	fta_reserve(self, self->size * 3 + 10);
-	TMP_STRING_APPEND(ans, "[|");
-	iterator = ARRAY_START(self);
-	tmp = f(iterator);
-	TMP_STRING_APPEND(ans, tmp);
+	*ans = NEW_STRING;
+	fta_reserve(ans, self->size * 3);
+	STRING_APPEND(ans, "[");
+	iterator = ARRAY_ITERATOR(self);
 	while (ARRAY_HASNEXT(self, iterator))
 	{
-		TMP_STRING_APPEND(ans, ", ");
 		tmp = f(iterator);
-		TMP_STRING_APPEND(ans, tmp);
+		STRING_APPEND(ans, tmp);
+		free(tmp);
+		STRING_APPEND(ans, ", ");
 	}
-	fta_append(ans, "|]", 3);
+	fta_popback(ans, 2);
+	fta_append(ans, "]", 2);
 	return ((char *)ans->data);
 }
-
-#undef TMP_STRING_APPEND

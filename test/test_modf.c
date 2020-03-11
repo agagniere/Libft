9af6894e063ccf8cd83ad43611d6cc395ab8ad23
Antoine Gagniere <antoine.gagniere@yahoo.com>
2020-03-11 16:49:48 +0100

    modfl test fixed

--- test/test_modf.c
+++ test/test_modf.c
@@ -26,7 +26,7 @@ bool test_modfl(long double f)
     long double intg[2];
     long double frac[2];
 
-    frac[0] = ft_modff(f, intg);
-    frac[1] = modff(f, intg + 1);
+    frac[0] = ft_modfl(f, intg);
+    frac[1] = modfl(f, intg + 1);
     return (frac[0] == frac[1] && intg[0] == intg[1]);
 }

17ae390773f71631776c552472c6c15fcebdcdfd
Antoine GAGNIERE <sid.xxdzs@yahoo.com>
2020-03-11 22:58:20 +0100

    Formatting fix

--- test/test_modf.c
+++ test/test_modf.c
@@ -4,36 +4,36 @@
 
 bool test_modf(double f)
 {
-    double intg[2];
-    double frac[2];
+	double intg[2];
+	double frac[2];
 
-    frac[0] = ft_modf(f, intg);
-    frac[1] = modf(f, intg + 1);
+	frac[0] = ft_modf(f, intg);
+	frac[1] = modf(f, intg + 1);
 	if (frac[0] == NAN && frac[1] == NAN)
 		return true;
-    return (frac[0] == frac[1] && intg[0] == intg[1]);
+	return (frac[0] == frac[1] && intg[0] == intg[1]);
 }
 
 bool test_modff(float f)
 {
-    float intg[2];
-    float frac[2];
+	float intg[2];
+	float frac[2];
 
-    frac[0] = ft_modff(f, intg);
-    frac[1] = modff(f, intg + 1);
+	frac[0] = ft_modff(f, intg);
+	frac[1] = modff(f, intg + 1);
 	if (frac[0] == NAN && frac[1] == NAN)
 		return true;
-    return (frac[0] == frac[1] && intg[0] == intg[1]);
+	return (frac[0] == frac[1] && intg[0] == intg[1]);
 }
 
 bool test_modfl(long double f)
 {
-    long double intg[2];
-    long double frac[2];
+	long double intg[2];
+	long double frac[2];
 
-    frac[0] = ft_modfl(f, intg);
-    frac[1] = modfl(f, intg + 1);
+	frac[0] = ft_modfl(f, intg);
+	frac[1] = modfl(f, intg + 1);
 	if (frac[0] == NAN && frac[1] == NAN)
 		return true;
-    return (frac[0] == frac[1] && intg[0] == intg[1]);
+	return (frac[0] == frac[1] && intg[0] == intg[1]);
 }

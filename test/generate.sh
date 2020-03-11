6b74603f4d9cfc1d00e30218a6e69a8d13dae4f8
Antoine Gagniere <antoine.gagniere@yahoo.com>
2020-03-11 14:51:18 +0100

    Update the test generation

--- test/generate.sh
+++ test/generate.sh
@@ -32,8 +32,8 @@ generate()
 	echo -e '#include "libunit.h"' > $launcher
 	echo -e "#include \"$header\"\n" >> $launcher
 	echo -e "int ${fun}_launcher(void)\n{" >> $launcher
-	echo -e "\tt_tests\t\ttest_list[1];" >> $launcher
-	echo -e "\n\ttest_list[0] = NEW_TESTS;" >> $launcher
+	echo -e "\tt_tests test_list[1];\n" >> $launcher
+	echo -e "\ttest_list[0] = NEW_TESTS;" >> $launcher
 
 	count=1
 	for input in $@
@@ -43,6 +43,7 @@ generate()
 		filename=$(printf "%.2i" $count)_${name}.c
 		echo -e "\t$filename"
 		echo -e "#include \"$header\"" > $filename
+		echo -e '#include "comparisons.h"' >> $filename
 		echo -e '#include "libft.h"' >> $filename
 		echo -e "#include <$lib.h>\n" >> $filename
 		echo -e "int ${name}(void)\n{" >> $filename
@@ -59,13 +60,13 @@ generate()
 	cd ..
 }
 
-generate "strlen" "int" "string" 'basic_string|"Hello\040World"' 'empty_string|""' 'other|"\\\t!@#$%^&\\\0*()"'
+generate "strlen" "int" "string" 'basic_string|"Hello\040World"' 'empty_string|""' 'other|"\\t!@#$%^&\\0*()"'
 
 generate "atoi" "int" "stdlib" 'basic_number|"28"' 'negative|"-8128"' 'empty|""' 'negative_zero|"-0"' \
-		 'space|"\040\040-496"' 'plus_sign|"+1729\040Ramanujan"' 'tab|"\040\\\t33550336\040Perfect"' \
-		 'carriage_return|"\\\r+877\040BellPrime"' 'form_feed|"\\\f\040-16127\040CarolPrime"' \
-		 'vertical_tab|"\\\v7057\040CubanPrime"' 'two_plus_signs|"++3"' 'invalid_first_char|"~197\040Chen"' \
-		 'leading_zeros|"000231"' 'combo|"\040\\\r\\\v\\\n\040-00000987654321"' 'int_min|"-2147483648"' \
+		 'space|"\040\040-496"' 'plus_sign|"+1729\040Ramanujan"' 'tab|"\040\\t33550336\040Perfect"' \
+		 'carriage_return|"\\r+877\040BellPrime"' 'form_feed|"\\f\040-16127\040CarolPrime"' \
+		 'vertical_tab|"\\v7057\040CubanPrime"' 'two_plus_signs|"++3"' 'invalid_first_char|"~197\040Chen"' \
+		 'leading_zeros|"000231"' 'combo|"\040\\r\\v\\n\040-00000987654321"' 'int_min|"-2147483648"' \
 		 'int_max|"2147483647"'
 
 generate "strcmp" "int_sign" "string" 'pure_alpha|"first_string","second_string"' 'empty_s1|"","why"' 'empty_s2|"are",""' \

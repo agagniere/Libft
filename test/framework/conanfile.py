20b04b673767a42d2933c9ec44167ccd0cbbf258
Antoine GAGNIERE <antoine.gagniere@yahoo.com>
2021-11-09 21:27:44 +0100

    Create 3 conan packages

--- test/framework/conanfile.py
+++ test/framework/conanfile.py
@@ -1,41 +1,42 @@
 from conans import ConanFile
 from conans import AutoToolsBuildEnvironment
 
-class LibftConan(ConanFile):
-    name = "libft"
-    version = "2.0"
+class LibUnitConan(ConanFile):
+    name = "libunit"
+    version = "1.0"
     license = "MIT"
     author = "agagniere sid.xxdzs@gmail.com"
     url = "https://github.com/agagniere/Libft"
-    description = "Standard datastructures"
+    description = "Simplistic unit testing framework"
     topics = ()
     settings = "os", "compiler", "build_type", "arch"
     options = {
-        "shared": [True, False],
-        "fPIC": [True, False],
+        "fPIC" : [True, False],
         "optimisation": ['0', '1', '2', '3', 's', 'fast'],
         "debug": [True, False]}
     default_options = {
-        "shared": True,
         "fPIC": False,
         "optimisation": '2',
         "debug": True}
     generators = "make"
+    requires = ["libft/2.0@sid/release"]
+
 
     def source(self):
-        self.run(f"git clone {self.url} .")
+        self.run(f"git clone {self.url} tmpdir")
+        self.run("mv tmpdir/test/framework/* .")
+        self.run("rm -rf tmpdir")
 
     def build(self):
         autotools = AutoToolsBuildEnvironment(self)
         autotools.flags = [f"-O{self.options.optimisation}", "-nolibc"]
         if self.options.debug:
             autotools.flags += ["-g"]
-        autotools.make(args=["shared" if self.options.shared else "static", "-j"])
+        autotools.make()
 
     def package(self):
-        self.copy("*.h", dst="include", src="include")
-        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.h", dst="include")
         self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["ft"]
+        self.cpp_info.libs = ["unit"]

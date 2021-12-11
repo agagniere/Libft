176c1e9b0e0533d8f7860c45d2d709c61c2ce34c
Antoine GAGNIERE <antoine.gagniere@yahoo.com>
2021-12-11 21:45:13 +0100

    Revise string header

--- include/ft_string.h
+++ include/ft_string.h
@@ -13,7 +13,6 @@
 #pragma once
 
 #include "ft_array.h"
-#include "ft_string_legacy.h"
 
 /*
 ** |		----------===== String =====----------
@@ -22,7 +21,6 @@
 /*
 ** Vector<char> wraper
 */
-
 typedef t_array t_string;
 
 typedef struct s_substr t_substr;
@@ -43,15 +41,14 @@ struct s_substr
 ** Failsafe constructor, no memory is allocated.
 ** -
 */
-
-#define NEW_SUB(S) (t_substr){S, S == NULL ? 0 : ft_strlen(S)}
+#define SUBSTR(S) _SUBSTR(S, S == NULL ? 0 : ft_strlen(S))
+#define _SUBSTR(S, L) (t_substr){S, L}
 
 /*
 ** String::new
 ** -
 ** Failsafe constructor.
 */
-
 #define NEW_STRING NEW_ARRAY(char)
 
 /*
@@ -59,57 +56,34 @@ struct s_substr
 ** -
 ** Handy function to use a String as a char*
 */
-
 char* ft_string(t_string* str);
-
-/*
-** String::join(char*)
-** -
-** Appends a char* at the end of the given String.
-*/
-
-#define STR_JOIN_CS(S, CS, L) (fta_append(S, CS, L))
 
 /*
 ** String::join(SubString)
 ** -
 ** Appends a SubString at the end of the given String.
 */
-
-#define STR_JOIN(STR, SUB) fta_append(STR, (SUB)->str, (SUB)->len)
+#define STRING_APPEND(STR, SUB) fta_append(STR, (SUB)->str, (SUB)->len)
 
 /*
 ** String::insert(SubString)
 */
+#define STRING_INSERT(ST, SB, I) fta_insert(ST, (void*)(SB)->str, (SB)->len, I)
 
-#define STR_INSERT(ST, SB, I) fta_insert(ST, (void*)(SB)->str, (SB)->len, I)
-
-/*
-** String::insert(char*)
-** -
-** _S_ the String where data is inserted.
-** _CS_ a char* where the data is.
-** _L_ the number of char _CS_ is long.
-** _I_ the index where the data is to be inserted.
-*/
-
-#define STR_INSERT_CS(S, CS, L, I) (fta_insert(S, (void*)CS, L, I))
 
 /*
 ** String::get
 ** -
 ** Returns the address of the element at index _I_
 */
-
-#define STR_GET(STR, I) ARRAY_GETT(char, STR, I)
+#define STRING_GET(STR, I) ARRAY_GETT(char, STR, I)
 
 /*
 ** String::getChar
 ** -
 ** Returns the element of index _I_
 */
-
-#define STR_GETCHAR(STR, I) (*ARRAY_GETT(char, STR, I))
+#define STRING_GETCHAR(STR, I) (*ARRAY_GETT(char, STR, I))
 
 /*
 ** String::nullTerminate
@@ -119,38 +93,10 @@ char* ft_string(t_string* str);
 ** However size will not be affected.
 ** the post-condition is 'strlen(STR->data) <= STR->size'
 */
-
-#define STR_NULL_TERMINATE(STR) (fta_reserve(STR, 1) || FTSZ(STR))
-
-/*
-** String::cleanRest
-** -
-** sets the rest of the currently allocated array to 0.
-** So if only appends are performed, there's no need to call FTSZ.
-** However if max = size the string isn't null-terminated, hence
-** the nullTerminate function.
-*/
-
-#define STR_CLEAN_REST(STR) ft_bzero(ARRAY_END(S), (S)->max - (S)->size)
-
-/*
-** Tools for manipulating characters
-*/
-
-#define UPPER_CASE(C)  (C & ~(1 << 5))
-#define LOWER_CASE(C)  (C | (1 << 5))
-#define TOGGLE_CASE(C) (C ^ (1 << 5))
-
-#define IS_LOWER(C) ('a' <= C && C <= 'z')
-#define IS_UPPER(C) ('A' <= C && C <= 'Z')
-#define IS_ALPHA(C) (IS_LOWER(C) || IS_UPPER(C))
-#define IS_DIGIT(C) ('0' <= C && C <= '9')
-#define IS_ALNUM(C) (IS_ALPHA(C) || IS_DIGIT(C))
-#define IS_BLANK(C) (C == ' ' || C == '\t')
-#define IS_SPACE(C) (IS_BLANK(C) || C == '\v' || C == '\r')
+#define STRING_NULL_TERMINATE(STR) (fta_reserve(STR, 1) || _FTSZ(STR))
 
 /*
 ** |		----------===== private: =====----------
 */
 
-#define FTSZ(STR) (*(char*)ARRAY_END(STR) = '\0')
+#define _FTSZ(STR) (*(char*)ARRAY_END(STR) = '\0')

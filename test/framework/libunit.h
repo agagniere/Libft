0b047551010cca050572d2004c7884737ec22332
Antoine Gagniere <antoine.gagniere@yahoo.com>
2022-04-23 00:49:36 +0200

    Test lambdas

--- test/framework/libunit.h
+++ test/framework/libunit.h
@@ -1,20 +1,20 @@
 #pragma once
 
 #include "ft_array.h"
+#include "ft_prepro/lambda.h"
 
-typedef int (*t_fptr)(void);
 typedef struct s_test t_test;
 typedef t_array       t_tests;
 
 struct s_test
 {
 	const char* name;
-	t_fptr      function;
+	Func      function;
 };
 
 #define NEW_TEST(NAME, FUNCTION) \
 	(t_test) { NAME, FUNCTION }
 #define NEW_TESTS NEW_ARRAY(t_test)
 
-int load_test(t_array* list, const char* name, t_fptr function);
+int load_test(t_array* list, const char* name, Func function);
 int launch_tests(const char* name, t_array* list);

9cf95510e9fe9e9b251e0d2456be7660b2f2b20f
Antoine Gagniere <antoine.gagniere@yahoo.com>
2022-04-23 22:57:05 +0200

    include standard header

--- test/test_libc.c
+++ test/test_libc.c
@@ -1,17 +1,19 @@
 #include "libunit.h"
 #include "libft.h"
 
+#include <string.h>
+
 #define name(NAME, STRING) NAME
 #define condition(NAME, STRING) strlen(STRING) == ft_strlen(STRING)
 
-#define S !#$%%&()*+-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~
-#define T PP_STR(S S S S) ",'\""
-#define U T T T T
+#define S "!#$%%&()*+-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~'\", "
+#define T S S S S S S S S
+#define U T T T T T T T T
 
 TEST_SECTION(strlen, name, condition,
     (basic_string, "Hello, World !"),
     (empty_string, ""),
     (short_string, "1"),
     (long_string, "   \t   \v  \n    !@#$^&*(%%)   0123456789   [0123456789] <0123456789>   \n  \0   "),
-    (extra_long_string, U U U U)
+    (extra_long_string, U)
 )

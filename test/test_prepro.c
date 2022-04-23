02040200905234035637e1878f596af6301b16a0
Antoine Gagniere <antoine.gagniere@yahoo.com>
2022-04-23 19:49:18 +0200

    go further

--- test/test_prepro.c
+++ test/test_prepro.c
@@ -3,19 +3,24 @@
 #include "ft_prepro/lambda.h"
 
 #define TEST(condition) lambda(int, (){return !(condition);})
-#define TEST_MACRO(macro, result, ...) PP_STR(macro of ARG_COUNT(__VA_ARGS__)), TEST(macro(__VA_ARGS__) == result)
+#define TEST_MACRO(macro, args, result) PP_STR(macro of ARG_COUNT args), TEST(macro args == result)
 
-int test_prepro(void)
-{
-    t_tests tests __attribute__((cleanup(fta_clear))) = NEW_TESTS;
+#define LOAD_TEST(F, args) load_test(&tests, F args);
 
-    load_test(&tests, TEST_MACRO(MAX,   1,  -42, 0, 1));
-    load_test(&tests, TEST_MACRO(MAX, 346,  123, 345, 234, 346, 299));
-    load_test(&tests, TEST_MACRO(MAX,   0,  -99, -123, -345, -0, -234, -346, -1));
+#define TEST_SECTION(name, F, ...) \
+    int PP_CAT(test_, name)(void) \
+    { \
+        t_tests tests __attribute__((cleanup(fta_clear))) = NEW_TESTS; \
+        FOR(EACH(__VA_ARGS__), LOAD_TEST, F); \
+        return launch_tests(PP_STR(name), &tests); \
+    }
 
-    load_test(&tests, TEST_MACRO(MIN, -42,  -42, 0, 1));
-    load_test(&tests, TEST_MACRO(MIN,  -1,  42, 0, -1, 1));
-    load_test(&tests, TEST_MACRO(MIN, 345,  4123, 345, 1234, 346, 9299));
-    load_test(&tests, TEST_MACRO(MIN,   0,  99, 123, 345, 0, 234, 346, 1));
-    return launch_tests("prepro", &tests);
-}
+TEST_SECTION(prepro, TEST_MACRO,
+    (MAX, (-42, 0, 1), 1),
+    (MAX, (123, 345, 234, 346, 299), 346),
+    (MAX, (-99, -123, -345, -0, -234, -346, -1), 0),
+    (MIN, (-42, 0, 1), -42),
+    (MIN, (42, 0, -1, 1), -1),
+    (MIN, (4123, 345, 1234, 346, 9299), 345),
+    (MIN, (99, 123, 345, 0, 234, 346, 1), 0)
+)

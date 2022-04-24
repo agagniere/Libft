653ce4b54b5744dcb82c4fe34dcaa668f3610001
Antoine Gagniere <antoine.gagniere@yahoo.com>
2022-04-25 00:33:33 +0200

    no itoa in libc

--- test/test_itoa.c
+++ test/test_itoa.c
@@ -1,17 +1,18 @@
 #include "libunit.h"
+#include "libft.h"
 #include <limits.h>
 
-#define ITOA_condition(NAME, INT) strcmp(atoi(INT), ft_atoi(INT)) == 0
+#define ITOA_condition(NAME, INT) strcmp(PP_STR(INT), ft_itoa(INT)) == 0
 
 TEST_SECTION(itoa, extract_name, ITOA_condition,
-    (zero,     0),
-    (one,      1),
-    (basic,    42),
-    (char_min, CHAR_MIN),
-    (char_max, CHAR_MAX),
-    (short_min,SHRT_MIN),
-    (short_max,SHRT_MAX),
-    (int_min,  INT_MIN),
-    (int_max,  INT_MAX),
+    (zero,       0),
+    (one,        1),
+    (basic,      42),
+    (char_min,   -128),
+    (char_max,   127),
+    (short_min,  -32768),
+    (short_max,  32767),
+    (int_min,    -2147483648),
+    (int_max,    2147483647),
     (many_zeros, 1000000000)
 )

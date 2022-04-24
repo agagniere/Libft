3423aa000475f87e6e9906c0b8d4bd5ffa38e95c
Antoine Gagniere <antoine.gagniere@yahoo.com>
2022-04-25 00:01:53 +0200

    fix

--- test/framework/libunit.h
+++ test/framework/libunit.h
@@ -26,7 +26,7 @@ int launch_tests(const char* name, t_array* list);
     FOR(EACH(__VA_ARGS__), CREATE_FUNCTION, NAME, GET_NAME, WRITE_CONDITION) \
     int PP_CAT(test_, NAME)(void) \
     { \
-        t_tests tests __attribute__((cleanup(fta_clear))) = NEW_TESTS; \
+        t_tests tests __attribute__((cleanup(fta_clear))) = NEW_ARRAY(t_test); \
         FOR(EACH(__VA_ARGS__), LOAD_TEST, NAME, GET_NAME) \
         return launch_tests(PP_STR(NAME), &tests); \
     }

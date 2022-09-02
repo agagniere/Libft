b53a56a4ddf9579375b40283bb5f0a6f01448f14
Antoine <antoine.gagniere@amadeus.com>
2022-09-02 21:26:46 +0200

    Convert ctype functions output to boolean

--- test/test_ctype.c
+++ test/test_ctype.c
@@ -2,26 +2,22 @@
 #include "ft_ctype.h"
 #include "comparisons.h"
 #include "ft_prepro/functions.h"
-#include "ft_printf.h"
 
 #include <limits.h>
 #include <stdbool.h>
 #include <ctype.h>
 
-static bool test(FunctionType(int, int) reference, FunctionType(int, int) mine, const char* name)
+static bool test(FunctionType(int, int) reference, FunctionType(int, int) mine)
 {
     unsigned char c = 128;
 
     while (c --> 0)
-        if (reference(c) != mine(c))
-        {
-            ft_dprintf(2, "Difference for function %s : %hhi '%c'\n", name, c, c);
+        if (!reference(c) != !mine(c))
             return false;
-        }
     return true;
 }
 
-#define CTYPE_condition(NAME) test(NAME, MERGE(ft, NAME), PP_STR(NAME))
+#define CTYPE_condition(NAME) test(NAME, MERGE(ft, NAME))
 
 TEST_SECTION(ctype, extract_name, CTYPE_condition,
     (isalnum),

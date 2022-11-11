f499817d9abf62b476ae124badb5c1061a39b14c
Antoine <angagnie@student.42.fr>
2022-11-11 01:35:31 +0100

    Add a type-safe wrapper of va_arg

---
+++ include/ft_variadic.h
@@ -0,0 +1,50 @@
+#pragma once
+
+/**
+** @file ft_variadic.c
+** Typesafe wrapper around va_arg
+*/
+
+#include "ft_prepro/constraints.h"
+#include "ft_prepro/tools.h"
+#include "ft_string.h"
+
+#include <stdarg.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <wchar.h>
+
+typedef signed char        schar;
+typedef unsigned char      uchar;
+typedef unsigned short     ushort;
+typedef long long          llong;
+typedef long unsigned      ulong;
+typedef long long unsigned ullong;
+typedef void*              pointer;
+
+#define PromotedToInt      Interface(char, schar, short)
+#define PromotedToUnsigned Interface(uchar, ushort)
+#define PromotedToDouble   Interface(float)
+#define UnpromotedSigned   Interface(int, long, llong, intmax_t, ptrdiff_t)
+#define UnpromotedUnsigned Interface(unsigned, ulong, ullong, uintmax_t, size_t)
+#define Unpromoted         Interface(UnpromotedSigned, UnpromotedUnsigned, t_cstring, pointer, wint_t)
+
+#define FT_VARIADIC_TYPES  Interface(PromotedToInt, PromotedToUnsigned, PromotedToDouble, Unpromoted)
+
+/**
+** va_arg wrapper.
+** Handles default argument promotion
+*/
+#define ft_va_arg(VA_LIST, TYPE) FT_VARIADIC_NAME(TYPE)(VA_LIST)
+
+/** @name private */
+/** @{ */
+
+#define FT_VARIADIC_NAME(T) MERGE(ft, va, arg, T)
+
+#define FT_VARIADIC_DECLARE(IGNORED, T)			\
+	T FT_VARIADIC_NAME(T)(va_list list);
+
+FOR(EACH(FT_VARIADIC_TYPES), FT_VARIADIC_DECLARE)
+
+/** @} */
---
+++ src/ft_variadic.c
@@ -0,0 +1,19 @@
+#include "ft_variadic.h"
+
+#define FT_VARIADIC_DEFINE(IGNORED, T)   \
+	T FT_VARIADIC_NAME(T)(va_list list)  \
+	{                                    \
+		return va_arg(list, T);          \
+	}
+
+#define FT_VARIADIC_PROMOTION_DEFINE(T_in, T_out) \
+	T_out FT_VARIADIC_NAME(T_out)(va_list list)   \
+	{                                             \
+		return (T_out)va_arg(list, T_in);         \
+	}
+
+FOR(EACH(Unpromoted), FT_VARIADIC_DEFINE)
+
+FOR(EACH(PromotedToInt),      FT_VARIADIC_PROMOTION_DEFINE, int)
+FOR(EACH(PromotedToUnsigned), FT_VARIADIC_PROMOTION_DEFINE, unsigned)
+FOR(EACH(PromotedToDouble),   FT_VARIADIC_PROMOTION_DEFINE, double)

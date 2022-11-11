c65eef3f22a4207108868836ad23fa2ea54265c0
Antoine <angagnie@student.42.fr>
2022-11-11 02:52:47 +0100

    Use va_arg wrapper in ft_printf

--- src/ft_printf/ft_convert_characters.c
+++ src/ft_printf/ft_convert_characters.c
@@ -1,30 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_convert_2.c                                     :+:      :+:    :+:   */
+/*   ft_convert_characters.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: angagnie <angagnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/30 12:37:44 by angagnie          #+#    #+#             */
-/*   Updated: 2018/12/04 18:04:50 by angagnie         ###   ########.fr       */
+/*   Updated: 2022/11/11 18:04:50 by angagnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_prepro/tools.h"
 #include "ft_printf_private.h"
 #include "ft_wchar.h"
+#include "ft_variadic.h"
 
 #include <wchar.h>
 
 int pf_cv_s(t_modifier* m, t_string* out, va_list ap)
 {
-	char* arg;
-	int   ans;
+	char*  arg;
+	int    ans;
+	size_t len;
 
-	arg = va_arg(ap, char*);
+	arg = ft_va_arg(ap, t_cstring);
 	if (arg == NULL)
 		arg = "(null)";
-	ans = (m->precision >= 0 ? MIN(ft_strlen(arg), (size_t)m->precision) : ft_strlen(arg));
+	len = ft_strlen(arg);
+	ans = (m->precision >= 0 ? MIN(len, (size_t)m->precision) : len);
 	string_append(out, &SUBSTR(arg, ans));
 	return ans;
 }
@@ -34,7 +37,7 @@ int pf_cv_ws(t_modifier* m, t_string* out, va_list ap)
 	wchar_t* arg;
 	size_t   len;
 
-	arg = va_arg(ap, wchar_t*);
+	arg = (wchar_t*)ft_va_arg(ap, pointer);
 	if (arg == NULL)
 		arg = L"(null)";
 	fta_reserve(out, 4 * ft_wstrlen(arg));
@@ -51,7 +54,7 @@ int pf_cv_c(t_modifier* m, t_string* out, va_list ap)
 	char arg;
 
 	(void)m;
-	arg = (char)va_arg(ap, int);
+	arg = ft_va_arg(ap, char);
 	string_append(out, &SUBSTR(&arg, 1));
 	return 1;
 }
@@ -62,7 +65,7 @@ int pf_cv_wc(t_modifier* m, t_string* out, va_list ap)
 	size_t ans;
 
 	(void)m;
-	arg = va_arg(ap, wint_t);
+	arg = ft_va_arg(ap, wint_t);
 	fta_reserve(out, 4);
 	ans = ft_widetoa((char*)ARRAY_END(out), arg);
 	out->size += ans;

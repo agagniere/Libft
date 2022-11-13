bd17729793764ae6a0cb582a4de9be71ba837e83
Antoine <angagnie@student.42.fr>
2022-11-13 03:03:08 +0100

    rewrite printf tests

--- test/test_printf.c
+++ test/test_printf.c
@@ -1,172 +1,126 @@
-#include "ft_prepro/color.h"
-#include "ft_printf.h"
-#include "libft.h"
+#include "libunit.h"
 
+#include "ft_printf.h"
+#include "ft_string.h"
+#include "ft_prepro/raii.h"
+
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <limits.h>
+#include <stdbool.h>
 
-#define RUN_TEST(SUCCESS, FMT, ...)                                                               \
-	pf = asprintf(&pf_dst, FMT, ##__VA_ARGS__);                                                   \
-	ft = ft_asprintf(&ft_dst, FMT, ##__VA_ARGS__);                                                \
-	if (ft != pf)                                                                                 \
-		ft_printf("\t%sReturn value Real(%i vs %i)Yours%s\n", COLOR(RED), pf, ft, COLOR(NORMAL)); \
-	if (pf > 0 && ft > 0)                                                                         \
-	{                                                                                             \
-		if (ft_strcmp(ft_dst, pf_dst))                                                            \
-			ft_printf("%s\t|%s| (Real) vs\n\t|%s| (Yours)\n%s",                                   \
-			          COLOR(RED), pf_dst, ft_dst, COLOR(NORMAL));                                 \
-		else                                                                                      \
-		{                                                                                         \
-			ft_printf("\t[%s%s%s]\n", COLOR(GREEN), ft_dst, COLOR(NORMAL));                       \
-			SUCCESS;                                                                              \
-		}                                                                                         \
-	}                                                                                             \
-	else if (pf == 0 && ft == 0)                                                                  \
-		SUCCESS;                                                                                  \
-	else if (pf < 0 && ft > 0)                                                                    \
-		ft_printf("\t[%s%s%s]\n", COLOR(BLUE), ft_dst, COLOR(NORMAL));                            \
-	if (pf >= 0)                                                                                  \
-		free(pf_dst);                                                                             \
-	if (ft >= 0)                                                                                  \
-		free(ft_dst);
+void cleanup_t_cstring(t_cstring* str) { if (*str) free(*str); }
 
-#define TEST(FMT, ...)                       \
-	RUN_TEST(success++, FMT, ##__VA_ARGS__); \
-	total++;
+#define PRINTF_condition(NAME, ...)										\
+	({																	\
+		RAII(t_cstring) pf_dst = NULL;									\
+		RAII(t_cstring) ft_dst = NULL;									\
+		int pf = asprintf(&pf_dst, __VA_ARGS__);						\
+		int ft = ft_asprintf(&ft_dst, __VA_ARGS__);						\
+		bool ans = (ft == pf) || (ft < 0 && pf < 0);					\
+		if (ans && pf > 0)												\
+			ans &= (strcmp(ft_dst, ft_dst) == 0);						\
+		ans;															\
+	})
 
-// Testing Undefined behavior
-#define UB(FMT, ...) RUN_TEST(, FMT, __VA_ARGS__)
+TEST_SECTION(printf_string, extract_name, PRINTF_condition,
+			 (simplest,         "1 Simple String."),
+			 (simple,           "2 Stringception _%s_", "Hello World"),
+			 (length,           "3 Stringception _%20s_", "Hello World"),
+			 (precision,        "7 Stringception _%.20s_", "Hello World"),
+			 (right,            "4 Stringception _%-20s_", "Hello World"),
+			 (len_prec,         "5 Stringception _%20.5s_", "Hello World"),
+			 (len_prec_right,   "6 Stringception _%-20.5s_", "Hello World"),
+			 (prec_right,       "8 Stringception _%-.20s_", "Hello World"),
+			 (prec_len,         "9 Stringception _%5.20s_", "Hello World"),
+			 (len_zp,           "A Stringception _%20.s_", "Hello World"),
+			 (non_ascii,        "W Non ASCII string _%s_ (len = %li)", "我很好！", ft_strlen("我很好！")),
+			 (null,             "X NULL string _%s_", (char*)NULL),
+			 (null_length,      "Y NULL string _%4s_", (char*)NULL),
+			 (null_wide,        "Z NULL string _%S_", (wchar_t*)NULL),
+			 (null_length_wide, "10 NULL string _%4ls_", (wchar_t*)NULL),
+			 (wide_char,        "1D Wide char _%lc_", L'A'),
+			 (wide_char_alt,    "1E Wide char _%C_", L'B'),
+			 (simple_char,      "26 Sinple char _%c_", '^')
+	)
+
+TEST_SECTION(printf_hexa, extract_name, PRINTF_condition,
+			 (pf_H_Integer, "H Integer _%#.10hx_", -61234),
+			 (pf_I_Integer, "I Integer _%lX_", 0xDEADBEEFCAFE),
+			 (pf_J_Pointer, "J Pointer _%p_", &pf),
+			 (pf_K_Pointer, "K Pointer _%p_", (void*)1),
+			 (pf_L_Pointer, "L Pointer _%20p_", (void*)&ft_printf),
+			 (pf_M_Pointer, "M Pointer _%2p_", (void*)&ft_dprintf),
+			 (pf_R_Combo, "R Combo _%#20.10x_", '*'),
+			 (pf_V_Combo, "V Combo _%-#20.10x_", '*'),
+			 (pf_11_Number, "11 Number _%#015X_", 0xDEADBEEF),
+			 (pf_1B_Zero, "1B Zero %#x", 0),
+			 (pf_1C_Zero, "1C Zero %#.x %#.0x", 0, 0),
+			 (pf_1F_Hexa, "1F Hexa a%xb%xc%xd", 0, 55555, 1000000),
+			 (pf_1G_Hexa, "1G Hexa A%XB%XC%XD", 0, 55555, 1000000),
+			 (pf_1H_Octal, "1H Octal a%ob%oc%od", 0, 55555, 1000000),
+			 (pf_1I_Octal, "1I Octal _%o_%#o_%0o_%0#o_%.0o_%#.0o_", 0, 0, 0, 0, 0, 0)
+	)
+
+TEST_SECTION(printf_integer, extract_name, PRINTF_condition,
+			 (pf_B_Integer, "B Integer _%i_", -612345),
+			 (pf_C_Integer, "C Integer _%u_", (1 << 31) + 75312),
+			 (pf_D_Integer, "D Integer _%hhi_", -120),
+			 (pf_E_Integer, "E Integer _%hhu_", 250),
+			 (pf_F_Integer, "F Integer _%hi_", -9999),
+			 (pf_G_Integer, "G Integer _%hu_", 65123),
+			 (pf_N_Combo, "N Combo _%20i_", -'*'),
+			 (pf_O_Combo, "O Combo _%20.10i_", -'*'),
+			 (pf_P_Combo, "P Combo _%.10i_", -'*'),
+			 (pf_Q_Combo, "Q Combo _%020i_", -'*'),
+			 (pf_S_Combo, "S Combo _%-20i_", -'*'),
+			 (pf_T_Combo, "T Combo _%-20.10i_", -'*'),
+			 (pf_U_Combo, "U Combo _%-.10i_", -'*'),
+			 (pf_12_Zero, "12 Zero _%+i_", 0),
+			 (pf_13_Zero, "13 Zero _%0+i_", 0),
+			 (pf_14_Zero, "14 Zero _%+.i_", 0),
+			 (pf_15_Zero, "15 Zero _%.u_", 0),
+			 (pf_16_Zero, "16 Zero _%10.0i_", 0),
+			 (pf_17_Zero, "17 Zero _%10.0u_", 0),
+			 (pf_18_Zero, "18 Zero _%-10.0i_", 0),
+			 (pf_19_Advanced, "19 Advanced _%i_%10i_%.5i_%10.5i_%010i_%.0i_", -42, -42, -42, -42, -42, 0),
+			 (pf_1A_Advanced, "1A Advanced _%u_%10u_%.5u_%10.5u_%010u_%.0u_", -42, -42, -42, -42, -42, 0),
+			 (pf_1J_Wildcard, "1J Wildcard _%*s_%*s_", 5, "Hello world", 50, "Hello world"),
+			 (pf_1K_Wildcard, "1K Wildcard _%.*s_%.*s_", 5, "Hello world", 50, "Hello world"),
+			 (pf_1L_Wildcard, "1L Wildcard _%*.*s_%*.*s_", 5, 5, "Hello world", 50, 50, "Hello world"),
+			 (pf_1M_Wildcard, "1M Wildcard _%*.*s_%*.*s_", 50, 5, "Hello world", 5, 50, "Hello world"),
+			 (pf_1N_Wildcard, "1N Wildcard _%*.*s_%*.*s_", 50, -5, "Hello world", -5, 50, "Hello world"),
+			 (pf_1O_Long, "1O Long integers _%li_", (1L) << 50),
+			 (pf_1P_Long, "1P Long integers _%lli_", ((1L) << 60) + 12345),
+			 (pf_1Q_Long, "1Q Long integers _%lu_", ((1LU) << 63) + 98765)
+	)
+
+TEST_SECTION(printf_limits, extract_name, PRINTF_condition,
+			 (min_long_long, "1R Limits _%lli_", LLONG_MIN),
+			 (min_long,      "1S Limits _%li_", LONG_MIN),
+			 (min_int,       "1T Limits _%i_", INT_MIN),
+			 (min_short,     "1U Limits _%hi_", SHRT_MIN),
+			 (min_schar,     "1V Limits _%hhi_", SCHAR_MIN),
+			 (max_schar,     "1W Limits _%hhi_", SCHAR_MAX),
+			 (max_short,     "1X Limits _%hi_", SHRT_MAX),
+			 (max_int,       "1Y Limits _%i_", INT_MAX),
+			 (max_long,      "1Z Limits _%li_", LONG_MAX),
+			 (max_long_long, "20 Limits _%lli_", LLONG_MAX),
+			 (max_uchar,     "21 Limits unsigned _%hhu_", UCHAR_MAX),
+			 (max_ushort,    "22 Limits unsigned _%hu_", USHRT_MAX),
+			 (max_uint,      "23 Limits unsigned _%u_", UINT_MAX),
+			 (max_ulong,     "24 Limits unsigned _%lu_", ULONG_MAX),
+			 (max_ullong,    "25 Limits unsigned _%llu_", ULLONG_MAX)
+	)
 
 int test_printf()
 {
-	char* pf_dst;
-	char* ft_dst;
-	int   pf;
-	int   ft;
-	int   total   = 0;
-	int   success = 0;
+	int success = 0;
 
-	ft_printf("printf {\n");
-	TEST("1-Simple String.");
-	TEST("2-Stringception _%s_", "Hello World");
-	TEST("3-Stringception _%20s_", "Hello World");
-	TEST("4-Stringception _%-20s_", "Hello World");
-	TEST("5-Stringception _%20.5s_", "Hello World");
-	TEST("6-Stringception _%-20.5s_", "Hello World");
-	TEST("7-Stringception _%.20s_", "Hello World");
-	TEST("8-Stringception _%-.20s_", "Hello World");
-	TEST("9-Stringception _%5.20s_", "Hello World");
-	TEST("A-Stringception _%20.s_", "Hello World");
-	TEST("B-Integer _%i_", -612345);
-	TEST("C-Integer _%u_", (1 << 31) + 75312);
-	TEST("D-Integer _%hhi_", -120);
-	TEST("E-Integer _%hhu_", 250);
-	TEST("F-Integer _%hi_", -9999);
-	TEST("G-Integer _%hu_", 65123);
-	TEST("H-Integer _%#.10hx_", -61234);
-	TEST("I-Integer _%lX_", 0xDEADBEEFCAFE);
-	TEST("J-Pointer _%p_", &pf);
-	TEST("K-Pointer _%p_", (void*)1);
-	TEST("L-Pointer _%20p_", (void*)&ft);
-	TEST("M-Pointer _%2p_", (void*)&ft);
-	TEST("N-Combo _%20i_", -'*');
-	TEST("O-Combo _%20.10i_", -'*');
-	TEST("P-Combo _%.10i_", -'*');
-	TEST("Q-Combo _%020i_", -'*');
-	TEST("R-Combo _%#20.10x_", '*');
-	TEST("S-Combo _%-20i_", -'*');
-	TEST("T-Combo _%-20.10i_", -'*');
-	TEST("U-Combo _%-.10i_", -'*');
-	TEST("V-Combo _%-#20.10x_", '*');
-	TEST("W-Non ASCII string _%s_ (len = %li)", "我很好！", ft_strlen("我很好！"));
-	TEST("X-NULL string _%s_", (char*)NULL);
-	TEST("Y-NULL string _%4s_", (char*)NULL);
-	TEST("Z-NULL string _%S_", (wchar_t*)NULL);
-	TEST("10-NULL string _%4ls_", (wchar_t*)NULL);
-	TEST("11-Number _%#015X_", 0xDEADBEEF);
-	TEST("12-Zero _%+i_", 0);
-	TEST("13-Zero _%0+i_", 0);
-	TEST("14-Zero _%+.i_", 0);
-	TEST("15-Zero _%.u_", 0);
-	TEST("16-Zero _%10.0i_", 0);
-	TEST("17-Zero _%10.0u_", 0);
-	TEST("18-Zero _%-10.0i_", 0);
-	TEST("19-Advanced _%i_%10i_%.5i_%10.5i_%010i_%.0i_", -42, -42, -42, -42, -42, 0);
-	TEST("1A-Advanced _%u_%10u_%.5u_%10.5u_%010u_%.0u_", -42, -42, -42, -42, -42, 0);
-	TEST("1B-Zero %#x", 0);
-	TEST("1C-Zero %#.x %#.0x", 0, 0);
-	TEST("1D-Wide char _%lc_", L'A');
-	TEST("1E-Wide char _%C_", L'B');
-	TEST("1F-Hexa a%xb%xc%xd", 0, 55555, 1000000);
-	TEST("1G-Hexa A%XB%XC%XD", 0, 55555, 1000000);
-	TEST("1H-Octal a%ob%oc%od", 0, 55555, 1000000);
-	TEST("1I-Octal _%o_%#o_%0o_%0#o_%.0o_%#.0o_", 0, 0, 0, 0, 0, 0);
-	TEST("1J-Wildcard _%*s_%*s_", 5, "Hello world", 50, "Hello world");
-	TEST("1K-Wildcard _%.*s_%.*s_", 5, "Hello world", 50, "Hello world");
-	TEST("1L-Wildcard _%*.*s_%*.*s_", 5, 5, "Hello world", 50, 50, "Hello world");
-	TEST("1M-Wildcard _%*.*s_%*.*s_", 50, 5, "Hello world", 5, 50, "Hello world");
-	TEST("1N-Wildcard _%*.*s_%*.*s_", 50, -5, "Hello world", -5, 50, "Hello world");
-	TEST("1O-Long integers _%li_", (1L) << 50);
-	TEST("1P-Long integers _%lli_", ((1L) << 60) + 12345);
-	TEST("1Q-Long integers _%lu_", ((1LU) << 63) + 98765);
-	TEST("1R-Limits _%lli_", LLONG_MIN);
-	TEST("1S-Limits _%li_", LONG_MIN);
-	TEST("1T-Limits _%i_", INT_MIN);
-	TEST("1U-Limits _%hi_", SHRT_MIN);
-	TEST("1V-Limits _%hhi_", SCHAR_MIN);
-	TEST("1W-Limits _%hhi_", SCHAR_MAX);
-	TEST("1X-Limits _%hi_", SHRT_MAX);
-	TEST("1Y-Limits _%i_", INT_MAX);
-	TEST("1Z-Limits _%li_", LONG_MAX);
-	TEST("20-Limits _%lli_", LLONG_MAX);
-	TEST("21-Limits unsigned _%hhu_", UCHAR_MAX);
-	TEST("22-Limits unsigned _%hu_", USHRT_MAX);
-	TEST("23-Limits unsigned _%u_", UINT_MAX);
-	TEST("24-Limits unsigned _%lu_", ULONG_MAX);
-	TEST("25-Limits unsigned _%llu_", ULLONG_MAX);
-	TEST("26-Sinple char _%c_", '^');
-	TEST("27-Sinple char _%c_", '\t');
-	TEST("28-Sinple char _%c_", '\0');
-	TEST("")
-	TEST("%s%s", "", "")
-
-	int a, b, c, d, e, f;
-	TEST("~ N-Flag >%n< %10.7s %.10s %5i.%.4i%% >%n< %#X%x%X%x >%n<",
-	     &a, "outputs some secrets", "[REDACTED] your password", 99, 98765,
-	     &b, 0xb, 0xabe, 0xc, 0xafe, &c);
-	ft_printf("\t[~ N-Flag >%n< %10.7s %.10s %5i.%.4i%% >%n< %#X%x%X%x >%n<]\n",
-	       &a, "outputs some secrets", "[REDACTED] your password", 99, 98765,
-	       &b, 0xb, 0xabe, 0xc, 0xafe, &c);
-	printf("\t[~ N-Flag >%n< %10.7s %.10s %5i.%.4i%% >%n< %#X%x%X%x >%n<]\n",
-	       &d, "outputs some secrets", "[REDACTED] your password", 99, 98765,
-	       &e, 0xb, 0xabe, 0xc, 0xafe, &f);
-	fflush(stdout);
-	if (a != d || b != e || c != f)
-		ft_printf("%s\tFail: (Real,FT) (%i,%i) (%i,%i) (%i,%i)%s\n", COLOR(RED), d, a, e, b, f, c, COLOR(NORMAL));
-	else
-		success++;
-	total++;
-
-	UB("30-Wide string _%ls_", L"Hello there !");
-	UB("31-Wide string _%S_", L"克诺比将军");
-	UB("32-Wide char _%C_", 0x20AC);
-	UB("33-Wide char _%C_", L'€');
-	UB("34-Overflow _%i_", (42L << 50) +  987654L);
-	UB("35-Overflow _%u_", (42L << 50) +  987654L);
-	UB("36-Overflow _%hi_", -987654L);
-	UB("37-Overflow _%hu_", -987654L);
-	UB("38-Overflow _%hhi_", -987657L);
-	UB("39-Overflow _%hhu_", -987657L);
-	UB("3A-Overflow _%hhhi_", -987657L);
-	UB("3B-Overflow _%hhhu_", -987657L);
-	UB("3C-Overflow _%hhhhi_", -987657L);
-	UB("3D-Overflow _%hhhhu_", -987657L);
-
-	TEST("%s", "The End");
-	ft_printf("\t%s%i / %i%s tests passed\n}\n\n",
-	          (success == total ? COLOR(GREEN, BOLD) : COLOR(RED, BOLD)),
-	          success, total, COLOR(NORMAL));
-	return (success != total);
+	success += !test_printf_string();
+	success += !test_printf_integer();
+	success += !test_printf_hexa();
+	success += !test_printf_limits();
+	return !(success == 4);
 }
--- test/test_printf_manual.c
+++ test/test_printf_manual.c
@@ -7,10 +7,11 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-#define PRINTF_condition(NAME, ARGS, EXPECTED)               \
+#define _PRINTF_condition(NAME, ARGS, EXPECTED, EXPECTED_LEN)\
 	({                                                       \
-		int   expected_len = strlen(EXPECTED);               \
+		int   expected_len = EXPECTED_LEN;					 \
 		char* buffer       = NULL;                           \
 		int   len          = ft_asprintf(&buffer, ID ARGS);  \
 		bool  ans          = (len == expected_len);          \
@@ -20,8 +21,47 @@
 			ans &= (strcmp(buffer, EXPECTED) == 0);          \
 			free(buffer);                                    \
 		}                                                    \
+		if (!ans) printf ARGS;								 \
 		ans;                                                 \
 	})
+
+#define PRINTF_condition(NAME, ARGS, EXPECTED)						\
+	_PRINTF_condition(NAME, ARGS, EXPECTED, strlen(EXPECTED))
+
+
+TEST_SECTION(printf_wide, extract_name, _PRINTF_condition,
+			 (char,     ("40 - Wide char '%lc'", L'A'), "40 - Wide char 'A'", 18),
+			 (chinese,  ("41 - Non ASCII _%ls_", L"我很好！"), "40 - Non ASCII _我很好！_", 33)
+	)
+
+
+TEST_SECTION(printf_zero_int, extract_name, PRINTF_condition,
+			 (simple,         ("_%i_", 0),       "_0_"),
+			 (zero,           ("_%0i_", 0),      "_0_"),
+			 (dot,            ("_%.i_", 0),      "__"),
+			 (sign,           ("_%+i_", 0),      "_+0_"),
+			 (size,           ("_%5i_", 0),      "_    0_"),
+			 (precision,      ("_%.5i_", 0),     "_00000_"),
+			 (zero_precision, ("_%.0i_", 0),     "__"),
+			 (padding,        ("_%05i_", 0),     "_00000_"),
+			 (zp_size,        ("_%5.0i_", 0),    "_     _"),
+			 (zp_zero,        ("_%0.0i_", 0),    "__"),
+			 (zp_sign,        ("_%+.0i_", 0),    "_+_")
+	)
+
+TEST_SECTION(printf_zero_unsigned, extract_name, PRINTF_condition,
+			 (simple,         ("_%u_", 0),       "_0_"),
+			 (zero,           ("_%0u_", 0),      "_0_"),
+			 (dot,            ("_%.u_", 0),      "__"),
+			 (sign,           ("_%+u_", 0),      "_0_"),
+			 (size,           ("_%5u_", 0),      "_    0_"),
+			 (precision,      ("_%.5u_", 0),     "_00000_"),
+			 (zero_precision, ("_%.0u_", 0),     "__"),
+			 (padding,        ("_%05u_", 0),     "_00000_"),
+			 (zp_size,        ("_%5.0u_", 0),    "_     _"),
+			 (zp_zero,        ("_%0.0u_", 0),    "__"),
+			 (zp_sign,        ("_%+.0u_", 0),    "__")
+	)
 
 TEST_SECTION(printf_binary, extract_name, PRINTF_condition,
              (zero,      ("F0 - Binary _%b_", 0),      "F0 - Binary _0_"),
@@ -29,4 +69,4 @@ TEST_SECTION(printf_binary, extract_name, PRINTF_condition,
              (prefix,    ("F2 - Binary _%#b_", 9),     "F2 - Binary _b1001_"),
              (align,     ("F3 - Binary _%15b_", 753),  "F3 - Binary _     1011110001_"),
              (pad,       ("F4 - Binary _%015b_", 753), "F4 - Binary _000001011110001_")
-)
+	)
